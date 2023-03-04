edf93880791363e92f4073960422c98906781fce
Emad Kalalipour <emad.kalalipour@gmail.com>
2023-03-04 12:49:00 -0800

    new

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 const userRoute = require('./routes/userRoute');
 const userController = require('./controllers/userController');
 const authController = require('./controllers/authController');
-
+const chefRoute = require('./routes/chefRoute')
 
 
 //app.use(cookiePaser);
@@ -18,13 +18,11 @@ app.use(express.urlencoded({extended:true}));
 PUT MIDDLEWARE BELOW HERE
 */
 
-app.get('/api', (req, res) => {
-    res.status(200).json({message: 'success'});
-});
-
-app.get('/login', (req, res) => {
-  res.status(200).json({message: 'success'});
-});
+app.use('/api', userRoute);
+app.use('/api', chefRoute);
+// app.get('/login', (req, res) => {
+//   res.status(200).json({message: 'success'});
+// });
 
 
 /*
@@ -34,22 +32,16 @@ PUT MIDDLEWARE ABOVE HERE
 app.use((req, res) => res.status(404).send('Page not found'));
 app.use((err, req, res, next) => {
     const defaultErr = {
-        log: 'Express error handler caught unknown middleware error',
+        log: `Express error handler caught unknown middleware error: ${err.message}` ,
         status: 400,
         message: { err:'An error occurred' },
     }
     const errorObj = Object.assign(defaultErr, err);
+    console.error(errorObj.log)
     return res.status(errorObj.status).json(errorObj.message);
-})
+});
 
-// pool.connect((err, client, release) => {
-//   if (err) {
-//     console.error('Error acquiring client in DB', err.stack);
-//   } else {
-//     console.log('DB connected');
-//   }
-//   release();
-// });
+
 
 app.listen(PORT, () => {
     console.log(`rent a chef for your next meal right now @${PORT}`)
