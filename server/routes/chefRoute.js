c9759e4115787cfdd18e5cef80e7bacc503a7e9c
Emad Kalalipour <emad.kalalipour@gmail.com>
2023-03-05 14:36:06 -0800

    new

--- server/routes/chefRoute.js
+++ server/routes/chefRoute.js
@@ -2,9 +2,17 @@ const express = require('express');
 const chefController = require('../controllers/chefController');
 const router = express.Router();
 
-
 router.get('/getChefs', chefController.getChefs, (req, res) => {
   return res.status(200).json(res.locals.chefs)
 });
+
+router.post('/createBooking', chefController.createBooking, (req, res) =>{
+  return res.status(200).json(res.locals.booking)
+});
+
+router.post('/createProfile', chefController.createProfile, (req, res)=> {
+  return res.status(200).json(res.locals.profile)
+});
+
 
 module.exports = router;
