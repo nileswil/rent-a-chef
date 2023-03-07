9b89349fbaabd02492f05215b91fa93bb8aedc1f
Emad Kalalipour <emad.kalalipour@gmail.com>
2023-03-06 18:32:18 -0800

    new

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -7,9 +7,10 @@ const cookieController = require('../controllers/cookieController');
 
 router.post('/signup', userController.createUser, (req, res) => {
   res.status(200).json(res.locals.newUser);
-})
-router.post('/login', (req,res,next)=>{console.log("HELLOOOOO"); return next();}, authController.login, (req, res) => {
-  res.status(200).json({validate: res.locals.user}); 
+});
+
+router.post('/login', authController.login, (req, res) => {
+  return res.status(200).json({validate: res.locals.user}); 
 });
 
 //UPDATE A USER INFO
